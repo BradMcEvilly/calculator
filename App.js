e46c3c043feda3d8b2fd46ebb3b8af94308c7a11
Brad McEvilly <brad.mcevilly@gmail.com>
2020-10-22 16:42:02 -0400

    add initial javascript file to overall web app bundle that includes some initial placeholders

---
+++ App.js
@@ -0,0 +1,11 @@
+// Setup our event listeners
+// TO DO
+
+// Clear method based on functional specs - reset output display
+clear.onclick = () => {
+}
+
+// Calculate based on operation input
+compute = (operation) => {
+  let operator = operation.target.innerText;
+}
